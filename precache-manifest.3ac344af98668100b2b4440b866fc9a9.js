a377282f480187e8fa1666e4bd4a7ef975df2ad8
jdhilts <jameshilts@icloud.com>
2020-02-12 16:39:21 -0800

    Updates

---
+++ precache-manifest.3ac344af98668100b2b4440b866fc9a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88325a61cdc337b6c975a6e8036c970e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7a0128715cb8f3ae71",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "22bb5e40de61754f63a1",
+    "url": "/robofriends/static/css/main.f14344cc.chunk.css"
+  },
+  {
+    "revision": "2a7a0128715cb8f3ae71",
+    "url": "/robofriends/static/js/2.da6e5c15.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.da6e5c15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22bb5e40de61754f63a1",
+    "url": "/robofriends/static/js/main.32d8e47a.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
